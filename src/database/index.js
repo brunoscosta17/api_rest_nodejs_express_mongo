4cbfdd21d41bbd375ba08caf24528c794d7479de
= <=>
2019-04-20 13:38:41 -0300

    Implementing Authentication token

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/noderest_rocketseat', { useMongoClient: true });
+const uri = 'mongodb://localhost/noderest'
+mongoose.connect(uri, { useNewUrlParser: true })
+
+//mongoose.createConnection('mongodb://localhost/noderest', { useNewUrlParser: true });
+//mongoose.connect('mongodb://localhost/noderest_rocketseat', { useMongoClient: true });
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
