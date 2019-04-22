1af40334265bcb031dc58dfad932ad04a5b4fd6c
= <=>
2019-04-22 17:03:48 -0300

    Generate email service

--- src/app/controllers/authController.js
+++ src/app/controllers/authController.js
@@ -1,9 +1,13 @@
 const express = require('express');
-const router = express.Router();
-const User = require('../models/User');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const authConfig = require('../config/auth');
+const crypto = require('crypto');
+
+const authConfig = require('../../config/auth');
+
+const User = require('../models/User');
+
+const router = express.Router();
 
 function generateToken(params = {}) {
     return jwt.sign(params, authConfig.secret, {
@@ -43,6 +47,34 @@ router.post('/authenticate', async (req, res) => {
         user, 
         token:generateToken({ id: user.id }) 
     });
+});
+
+router.post('/forgot_password', async (req, res) => {
+    const { email } = req.body;
+
+    try {
+        const user = await User.findOne({ email });
+        
+        if (!user) 
+            return res.status(400).send({ error: 'User not found!' });
+            
+        const token = crypto.randomBytes(20).toString('hex');
+
+        const now = new Date();
+        now.setHours(now.getHours() + 1);
+
+        await User.findByIdAndUpdate(user.id, {
+            '$set': {
+                passwordResetToken: token,
+                passwordResetExpires: now
+            }
+        });
+
+        console.log(token, now);
+
+    } catch (error) {
+        res.status(400).send({ error: 'Error on forgot password. Try again.' })
+    }
 });
 
 module.exports = app => app.use('/auth', router);
